cb13ce8cc80d9509947e1e081c0ad05868b393ad
Engi1712 <glourung1712@gmail.com>
2024-04-16 18:29:03 +0300

    Player character sprite added

--- Characters/cat.gd
+++ Characters/cat.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 30
+@export var move_speed : float = 100
 @export var starting_direction : Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
@@ -11,16 +11,16 @@ func _ready():
 	#position = Vector2(100, 100)
 
 func _physics_process(_delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	).normalized()
+	#var input_direction = Vector2(
+	#	Input.get_action_strength("right") - Input.get_action_strength("left"),
+	#	Input.get_action_strength("down") - Input.get_action_strength("up")
+	#).normalized()
 	
-	update_animation_parameters(input_direction)
+	#update_animation_parameters(input_direction)
 	
-	velocity = input_direction * move_speed
+	#velocity = input_direction * move_speed
 	
-	move_and_slide()
+	#move_and_slide()
 	
 	pick_new_state()
 
