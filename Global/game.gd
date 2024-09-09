297773d6a4409e4410741f8f50a40d46e0d91e15
Engi1712 <glourung1712@gmail.com>
2024-09-09 09:34:48 +0300

    Minor changes

--- Global/game.gd
+++ Global/game.gd
@@ -11,9 +11,9 @@ func _ready():
 		#DisplayServer.window_set_current_screen(2)
 		DisplayServer.window_set_size(window_size)
 		DisplayServer.window_set_position(centered)
-	#else:
-		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		#get_window().borderless = true
-		#print(get_window().get_size_with_decorations())
-		#print(get_window().borderless)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		get_window().borderless = true
+		print(get_window().get_size_with_decorations())
+		print(get_window().borderless)
 	NavigationManager.call_deferred("go_to_level", "floor_2", "202")
